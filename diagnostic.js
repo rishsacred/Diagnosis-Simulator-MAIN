58e1635bffe5a71256c893923de587b4b1543c3b
Rishit <rishit.brahmbhatt08@gmail.com>
2025-08-03 13:58:55 -0400

    fixed bug

---
+++ diagnostic.js
@@ -0,0 +1,5 @@
+// Reset buttons to initial state
+orderLabsBtn.innerHTML = `<i class="fas fa-flask"></i> Order Tests`;
+orderLabsBtn.disabled = false;
+askMoreBtn.innerHTML = `<i class="fas fa-question-circle"></i> Ask More Questions`;
+askMoreBtn.disabled = false;
